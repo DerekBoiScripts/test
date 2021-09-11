1b8b82c184fc00c03a52c7cbb035a08369b4207e
jake666777666 <34963670+jake666777666@users.noreply.github.com>
2021-09-11 03:40:29 -0500

    Add files via upload

--- wip.ps1
+++ wip.ps1
@@ -1,11 +1,18 @@
 #The user gets to select the targets IP
 $ip = Read-Host -Prompt 'Enter the IP'
 
+
+#Testing certain ports, feel free to edit.
 Test-NetConnection -computername $ip -port 22
-timeout /t 3
+timeout /t 3 /nobreak
 Test-NetConnection -computername $ip -port 3389
-timeout /t 3
+timeout /t 3 /nobreak
 Test-NetConnection -computername $ip -port 646
-timeout /t 3
+timeout /t 3 /nobreak
 Test-NetConnection -computername $ip -port 5900
-timeout /t 3
+timeout /t 3 /nobreak
+Test-NetConnection -computername $ip -port 22222
+timeout /t 3 /nobreak
+
+#Finally, it pauses the script so you can see what happened.
+pause
