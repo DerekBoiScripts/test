f29ae9813cbe5516d2a3118d0d6f48970ff4766b
jake666777666 <34963670+jake666777666@users.noreply.github.com>
2021-09-12 22:51:27 -0500

    Add files via upload

--- wip.ps1
+++ wip.ps1
@@ -1,17 +1,27 @@
 #The user gets to select the targets IP
 $ip = Read-Host -Prompt 'Enter the IP'
 
+$menu = {
+    Read-Host -Prompt 'Do you want to do a [Full] Port Scan on the target? Or [Basic] Port Scan?'
+    if ($basic -eq "Basic") {
+        &$basic
+    } else {
+        Write-Host 'Error Close and reopen'
+    }
 
+}
+
+$basic
 #Testing certain ports, feel free to edit.
-Test-NetConnection -computername $ip -port 22
+Test-NetConnection -ComputerName $ip -port 22
 timeout /t 3 /nobreak
-Test-NetConnection -computername $ip -port 3389
+Test-NetConnection -ComputerName $ip -port 3389
 timeout /t 3 /nobreak
-Test-NetConnection -computername $ip -port 646
+Test-NetConnection -ComputerName $ip -port 646
 timeout /t 3 /nobreak
-Test-NetConnection -computername $ip -port 5900
+Test-NetConnection -ComputerName $ip -port 5900
 timeout /t 3 /nobreak
-Test-NetConnection -computername $ip -port 22222
+Test-NetConnection -ComputerName $ip -port 22222
 timeout /t 3 /nobreak
 
 #Finally, it pauses the script so you can see what happened.
